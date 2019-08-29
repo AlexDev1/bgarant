fd44ded78b5aa1655b43f5a724ae47430112ced2
Alex-iMac <alexbog80@gmail.com>
2019-08-29 16:20:46 +0500

    fix

--- bgarant/settings/production.py
+++ bgarant/settings/production.py
@@ -2,6 +2,15 @@ from .base import *
 
 DEBUG = False
 
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'amv@9yy8m#1o0@3^965$c#g%5#7p)$jnr7)silwm#r(!_xaroq'
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ['garant-bisness.ru']
+
+# EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+
 try:
     from .local import *
 except ImportError:
